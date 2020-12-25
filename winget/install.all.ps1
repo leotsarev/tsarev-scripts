74a617ae32d86ccc66fabba0fb4e7bf69fd80f63
Leonid Tsarev <leo@leotsarev.ru>
2020-12-25 11:54:08 +0300

    add install commands

---
+++ winget/install.all.ps1
@@ -0,0 +1,8 @@
+winget install Mozilla.FirefoxDeveloperEdition
+winget install Microsoft.Teams  
+winget install vscode
+winget install git
+winget install Microsoft.Powershell
+winget install LINQPad.LINQPad6
+winget install Microsoft.dotnet
+winget install Microsoft.VisualStudio.Enterprise
